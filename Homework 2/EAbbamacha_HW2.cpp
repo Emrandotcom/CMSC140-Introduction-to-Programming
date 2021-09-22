b444c8cd998c4796c82453c2dc5f9cccc2098448
Emran <63017060+Emrandotcom@users.noreply.github.com>
2021-09-21 20:37:38 -0400

    Uploaded cpp file

---
+++ Homework 2/EAbbamacha_HW2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Student name: Emran Abbamacha\n \tCMSC140, CRN33087\n" << endl;
+    cout << "My name is Emran Abbamacha" << endl;
+    cout << "I live on 1 Elm Street Silver Spring, MD 20906" << endl;
+    cout << "My phone number is (301-413-5396)" << endl;
+    cout << "My major is Computer Science" << endl;
+    return 0;
+}
